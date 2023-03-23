caab82bfb327fd7cb2b92bb28613dc55762d6c4c
Bernard Budano <bbudano@gmail.com>
2023-03-23 17:04:47 +0100

    Initial commit

---
+++ src/test/java/com/example/springvaadinflowdemo/SpringVaadinFlowDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvaadinflowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVaadinFlowDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
