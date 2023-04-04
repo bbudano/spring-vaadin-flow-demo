b0bd6bd12c08080edec739f42ff7f0b665a0bda2
Bernard Budano <bbudano@gmail.com>
2023-04-04 20:24:36 +0200

    Added theme toggle button

--- src/main/java/com/example/springvaadinflowdemo/components/CustomLayout.java
+++ src/main/java/com/example/springvaadinflowdemo/components/CustomLayout.java
@@ -6,6 +6,8 @@ import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -15,6 +17,8 @@ import com.vaadin.flow.theme.lumo.LumoUtility;
 public class CustomLayout extends AppLayout {
 
     private final transient SecurityService securityService;
+
+    private final Button themeToggleBtn = new Button();
 
     public CustomLayout(SecurityService securityService) {
         this.securityService = securityService;
@@ -31,17 +35,19 @@ public class CustomLayout extends AppLayout {
                 LumoUtility.FontSize.LARGE,
                 LumoUtility.Margin.MEDIUM);
 
+        themeToggleBtn.addClickListener(e -> toggleTheme());
+        themeToggleBtn.setIcon(new Icon(VaadinIcon.MOON));
+        themeToggleBtn.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+
         Button logout = new Button("Log out", e -> securityService.logout());
         logout.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
-        var header = new HorizontalLayout(new DrawerToggle(), title, logout);
+        var header = new HorizontalLayout(new DrawerToggle(), title, themeToggleBtn, logout);
 
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
         header.expand(title);
         header.setWidthFull();
-        header.addClassNames(
-                LumoUtility.Padding.Vertical.NONE,
-                LumoUtility.Padding.Horizontal.MEDIUM);
+        header.addClassNames(LumoUtility.Padding.Vertical.NONE, LumoUtility.Padding.Horizontal.MEDIUM);
 
         addToNavbar(header);
 
@@ -52,6 +58,18 @@ public class CustomLayout extends AppLayout {
                 new RouterLink("Dashboard", DashboardView.class),
                 new RouterLink("Customers", CustomerListView.class)
         ));
+    }
+
+    private void toggleTheme() {
+        if (getElement().getAttribute("theme").equalsIgnoreCase("dark")) {
+            themeToggleBtn.setIcon(new Icon(VaadinIcon.SUN_O));
+            themeToggleBtn.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+            getElement().setAttribute("theme", "light");
+        } else {
+            themeToggleBtn.setIcon(new Icon(VaadinIcon.MOON));
+            themeToggleBtn.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
+            getElement().setAttribute("theme", "dark");
+        }
     }
 
 }
